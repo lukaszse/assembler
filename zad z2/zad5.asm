17a13d8685a640bcb54412ff855b633295201ebc
lukaszse <llseremak@gmail.com>
2020-12-27 00:18:10 +0100

    poprawione zad 5

--- zad z2/zad5.asm
+++ zad z2/zad5.asm
@@ -14,14 +14,13 @@ start:
         jz koniec           ; skocz nfa koniec jeśli b=0
         mov ah, 8h 			; funkcja odczytu znaku z klawiatury bez echa i zapisu do rejestru al
         int 21h				; przerwanie dos
-        push ax             ; na stos
+        cmp al, ENTER_KEY   ; porownanie wpisanego znaku z kodem klawisza ENTER
+        je koniec           ; skok na koniec jesli wcisnieto enter
         mov dl, '*'         ; wpisanie * do dl    
         mov ah, 02h         ; funkcja wyświetlenia znaku
         int 21h             ; wywołanie przerrwania
         dec cx              ; dekrementacja bx
-        pop ax              ; ze stosu do ax
-        cmp al, ENTER_KEY   ; porownanie wpisanego znaku z kodem klawisza ENTER
-    jne petla               ; jeśli wciśnięty klawisz inny niż enter skocz do etykiety petla
+    jmp petla               ; jeśli wciśnięty klawisz inny niż enter skocz do etykiety petla
     koniec:
         mov	ax, 4c00h		; funkja zakończenia programu
         int	21h	
